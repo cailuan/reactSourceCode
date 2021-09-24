a60662d1cf1e2ffcefaad86b402c064e1c49b52b
cla <cla1136@163.com>
2021-09-24 08:59:54 +0800

    init

--- demo/src/demo_0.js
+++ demo/src/demo_0.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import  { useEffect, useLayoutEffect, useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
+  
+  useEffect(()=>{
+    console.log(count,"useEffect")
+  },[count])
+
+  useLayoutEffect(()=>{
+    console.log("useLayoutEffect")
+  },[])
+
   return (
-    <>
+    <div>
       <h1
         onClick={() => {
           // debugger;
           setCount(() => count + 1);
         }}
       >
-        <p title={count}>{count}</p> xiaochen
+        {count}
       </h1>
-    </>
+    </div>
   );
+}
+
+
+function Srp(){
+  return <div>srp</div>
 }
--- demo/src/index.js
+++ demo/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./demo_0";
 
 const rootEl = document.getElementById("root");
+const rootEl1 = document.getElementById("root1");
+console.log(ReactDOM,"ReactDOM")
+// ReactDOM.render(<App />, rootEl);
+ReactDOM.createRoot(rootEl).render(<App />);
 
-ReactDOM.render(<App />, rootEl);
-// ReactDOM.unstable_createRoot(rootEl).render(<App />);
+// ReactDOM.createRoot(rootEl1).render(<div > createRoot</div>);
